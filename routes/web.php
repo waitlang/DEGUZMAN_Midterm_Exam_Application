e7a9d36fe2e2156dde66c4168cb673149610e2ed
ShivTheGooseman <brensdeguzman7@gmail.com>
2025-02-20 21:07:35 +0800

    Added blade template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/user/{id}', [ProductController::class, 'show']);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['page' => 'GadgetBahns']);
 });
